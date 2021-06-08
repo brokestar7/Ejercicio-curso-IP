1a67ec07ae4f2a85573c91112f2a34bef73c2ffd
brokestar7 <82914463+brokestar7@users.noreply.github.com>
2021-06-08 15:39:59 +0200

    Acabando el ejercicio 19
    ya le añadi el prompt con las vidas que quiere el usuario y lo inicialize del todo por ahora

--- Ejercicio 19.js
+++ Ejercicio 19.js
@@ -2,12 +2,17 @@ let opcionHumano = 0;
 let opcionMaquina = 0;
 let contadorHumano = 5;
 let contadorMaquina = 5;
-let imgCorazones = "<img  id=\"corazon\" src='/Images/Corazon.png' alt='Corazon'></img>";
+let numeroDeVidas = 0;
+let imgCorazones = "<img  id=\"corazon\" src='Images/Corazon.png' alt='Corazon'></img>";
 function iniciarJuego(){
+    numeroDeVidas=parseInt(prompt("Introduce el numero de vidas."));
+    inicializarJuego();
+}
+function inicializarJuego() {
     opcionHumano = 0;
     opcionMaquina = 0;
-    contadorHumano = 5;
-    contadorMaquina = 5;
+    contadorHumano = numeroDeVidas;
+    contadorMaquina = numeroDeVidas;
     document.getElementById("mostrarImagenes").innerHTML="";
     document.getElementById("corazonesHumano").innerHTML ="";
     document.getElementById("corazonesMaquina").innerHTML ="";
@@ -17,16 +22,16 @@ function iniciarJuego(){
         document.getElementById("corazonesMaquina").innerHTML += imgCorazones;
     }
     for(i=1;i<=3;i++) {
-        document.getElementById("mostrarImagenes").innerHTML += "<img src='/Images/PPT"+i+".jpg' onclick='compararResultados("+i+")'>"; 
+        document.getElementById("mostrarImagenes").innerHTML += "<img src='Images/PPT"+i+".jpg' onclick='compararResultados("+i+")'>"; 
     }
 }
 function compararResultados(opcionEscogida){
     let opcionMaquina = Math.floor(Math.random() * 3)+1;
-    let mostrarImagenMaquina =  "<img src='/Images/PPT"+opcionMaquina+".jpg'>";
+    let mostrarImagenMaquina =  "<img src='Images/PPT"+opcionMaquina+".jpg'>";
     document.getElementById("imagenMaquina").innerHTML = mostrarImagenMaquina;
     console.log(opcionMaquina);
     if (opcionEscogida == opcionMaquina) {
-        document.getElementById("mostrarResultado").innerHTML = "<img src='/Images/PPT"+opcionEscogida+".jpg'>" + "<span id='vs'>vs</span>" + mostrarImagenMaquina + " Es un empate!" ;  
+        document.getElementById("mostrarResultado").innerHTML = "<img src='Images/PPT"+opcionEscogida+".jpg'>" + "<span id='vs'>vs</span>" + mostrarImagenMaquina + " Es un empate!" ;  
 
     }
     else if( opcionEscogida<opcionMaquina&&opcionEscogida<3&&opcionEscogida>1) {//opcion escogida=2 vs 3
@@ -35,7 +40,7 @@ function compararResultados(opcionEscogida){
         for(i=0;i<contadorHumano;i++) {
         document.getElementById("corazonesHumano").innerHTML += imgCorazones;
         }
-        document.getElementById("mostrarResultado").innerHTML = "<img src='/Images/PPT"+opcionEscogida+".jpg'>" + "<span id='vs'>vs</span>" + mostrarImagenMaquina + " El ganador es la Maquina!" ; 
+        document.getElementById("mostrarResultado").innerHTML = "<img src='Images/PPT"+opcionEscogida+".jpg'>" + "<span id='vs'>vs</span>" + mostrarImagenMaquina + " El ganador es la Maquina!" ; 
 
     }
     else if( opcionEscogida>opcionMaquina&&opcionEscogida<3&&opcionEscogida>1){//opcion escogida=2vs 1
@@ -45,7 +50,7 @@ function compararResultados(opcionEscogida){
         document.getElementById("corazonesMaquina").innerHTML += imgCorazones;
         }
         console.log("funciona putito version else if 2");
-        document.getElementById("mostrarResultado").innerHTML = "<img src='/Images/PPT"+opcionEscogida+".jpg'>" + "<span id='vs'>vs</span>" + mostrarImagenMaquina + " El ganador es el Humano!";
+        document.getElementById("mostrarResultado").innerHTML = "<img src='Images/PPT"+opcionEscogida+".jpg'>" + "<span id='vs'>vs</span>" + mostrarImagenMaquina + " El ganador es el Humano!";
     }
     else if(opcionEscogida<opcionMaquina&&opcionEscogida<2&&opcionMaquina<3){//1vs2
         document.getElementById("corazonesHumano").innerHTML = "";
@@ -53,7 +58,7 @@ function compararResultados(opcionEscogida){
         for(i=0;i<contadorHumano;i++) {
         document.getElementById("corazonesHumano").innerHTML += imgCorazones;
         }
-        document.getElementById("mostrarResultado").innerHTML = "<img src='/Images/PPT"+opcionEscogida+".jpg'>" + "<span id='vs'>vs</span>" + mostrarImagenMaquina + " El ganador es la Maquina!";
+        document.getElementById("mostrarResultado").innerHTML = "<img src='Images/PPT"+opcionEscogida+".jpg'>" + "<span id='vs'>vs</span>" + mostrarImagenMaquina + " El ganador es la Maquina!";
     }
     else if(opcionEscogida<opcionMaquina&&opcionEscogida<2&&opcionMaquina>2) {//1vs3
         document.getElementById("corazonesMaquina").innerHTML = "";
@@ -61,7 +66,7 @@ function compararResultados(opcionEscogida){
         for(i=0;i<contadorMaquina;i++) {
         document.getElementById("corazonesMaquina").innerHTML += imgCorazones;
         }
-        document.getElementById("mostrarResultado").innerHTML = "<img src='/Images/PPT"+opcionEscogida+".jpg'>" + "<span id='vs'>vs</span>" + mostrarImagenMaquina + " El ganador es el Humano!";
+        document.getElementById("mostrarResultado").innerHTML = "<img src='Images/PPT"+opcionEscogida+".jpg'>" + "<span id='vs'>vs</span>" + mostrarImagenMaquina + " El ganador es el Humano!";
     }
     else if(opcionEscogida>opcionMaquina&&opcionEscogida>2&&opcionMaquina>1) {//3vs2
         document.getElementById("corazonesMaquina").innerHTML = "";
@@ -69,7 +74,7 @@ function compararResultados(opcionEscogida){
         for(i=0;i<contadorMaquina;i++) {
         document.getElementById("corazonesMaquina").innerHTML += imgCorazones;
         }
-        document.getElementById("mostrarResultado").innerHTML = "<img src='/Images/PPT"+opcionEscogida+".jpg'>" + "<span id='vs'>vs</span>" + mostrarImagenMaquina + " El ganador es el Humano!";
+        document.getElementById("mostrarResultado").innerHTML = "<img src='Images/PPT"+opcionEscogida+".jpg'>" + "<span id='vs'>vs</span>" + mostrarImagenMaquina + " El ganador es el Humano!";
     }
     else{
         document.getElementById("corazonesHumano").innerHTML = "";
@@ -77,11 +82,15 @@ function compararResultados(opcionEscogida){
         for(i=0;i<contadorHumano;i++) {
         document.getElementById("corazonesHumano").innerHTML += imgCorazones;
         }
-        document.getElementById("mostrarResultado").innerHTML = "<img src='/Images/PPT"+opcionEscogida+".jpg'>" + "<span id='vs'>vs</span>" + mostrarImagenMaquina + " El ganador es la Maquina!";  
+        document.getElementById("mostrarResultado").innerHTML = "<img src='Images/PPT"+opcionEscogida+".jpg'>" + "<span id='vs'>vs</span>" + mostrarImagenMaquina + " El ganador es la Maquina!";  
     }        
-    if(contadorHumano==0||contadorMaquina==0){
-    alert("¡Se acabo el Juego! ¡Bien Jugado!");
-    iniciarJuego();
+    if(contadorHumano==0){
+        alert("¡Lo siento has Perdido! ¡Bien Jugado");
+        inicializarJuego();
+    }
+    if(contadorMaquina==0){
+        alert("¡Felicidades has Ganado! ¡Bien Jugado!");
+        inicializarJuego();
     }
 
 }
